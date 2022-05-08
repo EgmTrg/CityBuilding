7cbfb7f98c1ef0e117d4cdc1c38fd486dba5d1aa
Egemen Turoglu <62505792+EgmTrg@users.noreply.github.com>
2022-05-08 17:56:53 +0300

    +added: Singleton Pattern. +added: Collects the resource object when clicked.

---
+++ City/Assets/Scripts/MonoSingleton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace CityBuilding.Managers
+{
+    public class MonoSingleton<T> : MonoBehaviour where T: MonoSingleton<T>
+    {
+        private static volatile T instance;
+
+        public static T Instance
+        {
+            get {
+                if (instance == null) {
+                    instance = GameObject.FindObjectOfType(typeof(T)) as T;
+                }
+                return instance;
+            }
+        }
+
+    }
+}
---
+++ City/Assets/Scripts/ObstacleObject.cs
@@ -0,0 +1,34 @@
+﻿using CityBuilding.Managers;
+using UnityEngine;
+
+namespace CityBuilding.Resources
+{
+    public class ObstacleObject : MonoBehaviour
+    {
+        public enum ObstacleType { Wood, Rock }
+        public ObstacleType obstacleType;
+        public int resourceAmount = 10;
+
+        private void Awake() {
+            Debug.LogWarning("resourceAmont aşağıdaki şekilde de olabilir.");
+            // resourceAmount = Random.Range(2, 10);
+            // ya da 
+            // resourceAmount = (int)(Random.value * 10f);
+        }
+
+        private void OnMouseDown() {
+            Debug.Log($"Clicked to {gameObject.name}");
+
+            switch (obstacleType) {
+                case ObstacleType.Wood:
+                    ResourceManager.Instance.AddWood(resourceAmount);
+                    break;
+                case ObstacleType.Rock:
+                    ResourceManager.Instance.AddRock(resourceAmount);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
